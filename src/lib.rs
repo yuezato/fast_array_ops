b925f66ca848e29801ec00e48391b92ec7ab8f84
yuuya uezato <yuuya_uezato@dwango.co.jp>
2020-07-15 21:43:14 +0900

    fix clippy error againt codes generated by bindgen

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 #![allow(dead_code)]
+#![allow(clippy::redundant_static_lifetimes)]
 
 include!(concat!(env!("OUT_DIR"), "/array_ops.rs"));
 
@@ -9,7 +10,7 @@ pub fn fast_array_copy(dst: &mut [u8], src: &[u8]) {
     debug_assert!(dst.len() == src.len());
 
     let len = src.len() as u32;
-    
+
     unsafe {
         let dst = dst.as_mut_ptr();
         let src = src.as_ptr();
@@ -21,7 +22,7 @@ pub fn fast_array_xor(dst: &mut [u8], src: &[u8]) {
     debug_assert!(dst.len() == src.len());
     let len = src.len() as u32;
     debug_assert!(len % 64 == 0);
-    
+
     unsafe {
         let dst = dst.as_mut_ptr();
         let src = src.as_ptr();
